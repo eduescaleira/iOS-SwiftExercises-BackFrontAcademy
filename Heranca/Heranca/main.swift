ca9ff0586866abd4ad7bf82f57be1fa3280e73f6
Eduardo Escaleira <duescaleira@gmail.com>
2023-03-09 21:37:38 -0300

    no message

--- Heranca/Heranca/main.swift
+++ Heranca/Heranca/main.swift
@@ -7,5 +7,48 @@
 
 import Foundation
 
-print("Hello, World!")
+// MARK: HERANÇA
+// Classe pai / Super Class
+
+class Animal {
+    
+    var idade: Int = 10
+    
+    func comer() {
+        print("O animal está comendo!")
+    }
+}
+
+class Cavalo: Animal {
+    
+}
+
+var meuCavalo: Cavalo = Cavalo()
+meuCavalo.comer()
+
+
+// classe pai
+
+class Pessoa {
+    var nome: String
+    
+    init(nome: String) {
+        self.nome = nome
+    }
+}
+
+class Caio: Pessoa {
+    var altura: Double
+    
+    init(altura: Double, nome: String) {
+        self.altura = altura
+        super.init(nome: nome) // usado quando a classe pai tem um construtor
+    }
+    
+}
+
+//var meuNome: Caio = Caio(nome: "Caio")
+
+
+
 

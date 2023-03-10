9aafcd1f432231c325fca9c0e44e5fb2771f4919
Eduardo Escaleira <duescaleira@gmail.com>
2023-03-09 21:48:00 -0300

    desafios concluidos

--- Heranca/Heranca/main.swift
+++ Heranca/Heranca/main.swift
@@ -51,4 +51,52 @@ class Caio: Pessoa {
 
 
 
+// DESAFIO PARA PASSAR PARA CASA
 
+// Criar uma classe pai na qual tenha 3 caracteristicas e 2 ações.
+// Uma das classes filhas não deve conter construtor.
+
+class Aircraft {
+    var type: String
+    var capacity: Int
+    var range: Double
+    
+    func startEngine() {
+        print("Engines on!")
+    }
+    
+    func depart() {
+        print("Departed!")
+    }
+    
+    init(type: String, capacity: Int, range: Double) {
+        self.type = type
+        self.capacity = capacity
+        self.range = range
+    }
+}
+
+// 2. Criar 2 classes filhas distintas, porem ambas vão herdar da classe pai(super).
+// Cada classe filha terá 3 caracteristicas especificas.
+// Uma das classes filhas deve conter contrutor indicando os valores de todas as suas propriedades.
+
+class Robinson22: Aircraft {
+    var fuelCapacity: Double
+    var pilots: Int
+    var fuelType: String
+    
+    init(fuelCapacity: Double, pilots: Int, fuelType: String, type: String, capacity: Int, range: Double) {
+        self.fuelCapacity = fuelCapacity
+        self.pilots = pilots
+        self.fuelType = fuelType
+        super.init(type: type, capacity: capacity, range: range)
+    }
+    
+}
+
+class CJ2: Aircraft {
+    var wings: Int = 2
+    var maxCeilling: Double = 45000
+    var cruiseSpeed: Double = 450.80
+    
+}

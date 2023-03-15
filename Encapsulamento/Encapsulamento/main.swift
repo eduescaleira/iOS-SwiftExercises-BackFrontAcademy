dfa1a3f2c7f7b7b60367033c8d0d6e095656f4dc
Eduardo Escaleira <duescaleira@gmail.com>
2023-03-15 08:11:28 -0300

    no message

--- Encapsulamento/Encapsulamento/main.swift
+++ Encapsulamento/Encapsulamento/main.swift
@@ -7,5 +7,26 @@
 
 import Foundation
 
-print("Hello, World!")
+class Carro {
+    
+    private var modelo: String
+    
+    init(modelo: String) {
+        self.modelo = modelo
+    }
+    
+    public func getModelo() -> String {
+        return self.modelo
+    }
+    
+    public func setModelo(modelo: String) {
+        self.modelo = modelo
+    }
+    
+}
 
+var meuCarro: Carro = Carro(modelo: "BMW 320i")
+
+print(meuCarro.getModelo())
+meuCarro.setModelo(modelo: "La Ferrari")
+print(meuCarro.getModelo())

c84478cdb3e3ba469722563f0d2337efc0baa210
Eduardo Escaleira <duescaleira@gmail.com>
2023-04-06 23:02:38 -0300

    no message

---
+++ PrimeiroAppTableView/PrimeiroAppTableView/Cell/NameTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  NameTableViewCell.swift
+//  PrimeiroAppTableView
+//
+//  Created by Eduardo Escaleira on 06/04/23.
+//
+
+import UIKit
+
+class NameTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+ 
+}
---
+++ PrimeiroAppTableView/PrimeiroAppTableView/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  PrimeiroAppTableView
+//
+//  Created by Eduardo Escaleira on 06/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configTableView()
+    }
+
+    private func configTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}

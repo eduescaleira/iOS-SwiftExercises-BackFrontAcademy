c86e62f4bc59371cf729936292e2392d4f01a8d4
Eduardo Escaleira <duescaleira@gmail.com>
2023-04-20 20:42:37 -0300

    no message

---
+++ PrimeiroProjetoCollectionView/PrimeiroProjetoCollectionView/Cell/CustomCollectionViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  CustomCollectionViewCell.swift
+//  PrimeiroProjetoCollectionView
+//
+//  Created by Eduardo Escaleira on 20/04/23.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var carImageView: UIImageView!
+    
+//    static let identifier: String = "CustomCollectionViewCell" -> EVITAR STRINGS MÁGICAS
+    static let identifier: String = String(describing: CustomCollectionViewCell.self) //
+    
+    static func nib() -> UINib {
+        return UINib(nibName: identifier, bundle: nil)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func setupCell(nameImage: String) {
+        carImageView.image = UIImage(named: nameImage)
+    }
+    
+
+}
